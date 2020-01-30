cc4fb3c8513b3502e6ae151467ddfa6b1cc418d4
Yasin Gunes <57527083+Yasin-VU@users.noreply.github.com>
2020-01-30 09:04:58 +0100

    Update examples.py
    added more info to examples,
    adjusted goal to help user a bit more with non-general-python stuff like API's and wrappers,
    adding a general python course is OUT OF SCOPE

--- examples.py
+++ examples.py
@@ -3,6 +3,35 @@
 # Let's get started
 
 # example 0: your first dataset
+# it is known bibliometrics data exists and is out there, but it needs to get on the computer
+# preferably without any manual actions like visiting websites and pressing download buttons
+# for this purpose, one can use API's (Application Programmable Interfaces) and download data directly without leaving Python (hurray)
+# below a first dataset using Scopus
+#
+# first one must define a search query to tell Scopus which data is wanted (the entire corpus is simply too large)
+# this query asks Scopus to return records of publications with 'Gunes' in the author names and 'tensor' in the title.
+my_query = "AUTHOR(Gunes) AND TITLE(TENSOR)"  
 # 
+# Next this query must be sent to Scopus, through the API
+# There are two ways to do this: either by using general API commands or by using a 'wrapper'.
+# The 'wrapper' is a package of functions which simplifies the general API commands
+# One wrapper for scopus search is the following
+
+
+# below = under construction
+
+import ScopusSearch
+# and it allows really easy querying of Scopus
+s = ScopusSearch(my_query, refresh=True)
+
+
+# download package
+# imports?
+# setup config
+# api keys
+
+df = pd.DataFrame(s.results)
+
+
 
 # example 1
