ec64d5946cffbe413105cec3bb3c0c5550f1d3df
Yasin Gunes <57527083+Yasin-VU@users.noreply.github.com>
2020-01-30 08:51:22 +0100

    Create examples.py

---
+++ examples.py
@@ -0,0 +1,8 @@
+# Welcome to examples.py with examples to get started with this package
+# The goal of this package is to provide you user-friendly commands to download and prepare bibliometrics data
+# Let's get started
+
+# example 0: your first dataset
+# 
+
+# example 1
